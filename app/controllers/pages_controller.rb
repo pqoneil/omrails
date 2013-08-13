35c26f958aae13ecb5f3ed61af18928d9e7b6166
Patrick O'Neil <patrickqoneil@gmail.com>
2013-08-13 16:52:24 -0700

    Added partials, layouts, about us page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def action
+  	#content
+  	#Rules
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Omrails::Application.routes.draw do
-  get "pages/home"
+
+get'about' => 'pages#about'
+
+root 'pages#home'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
