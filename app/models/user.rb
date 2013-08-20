aa8dd8680255e9e8bc83de6eaa9b0d5a685ade5d
Patrick O'Neil <patrickqoneil@gmail.com>
2013-08-20 01:56:43 -0700

    Add pin associations to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, # :recoverable,
          :rememberable, :trackable, :validatable
   validates_presence_of :name
+
+  has_many :pins, :dependent => :destroy
+
 end
